eafb3c8109c78305c6c8e7d9a3c1787a924ba29b
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-02-23 14:44:16 +0100

    Imported react-moralis

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import { useMoralis } from "react-moralis";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { authenticate, isAuthenticated, user } = useMoralis();
+
+  if (!isAuthenticated) {
+    return (
+      <div className="container pt-5 text-center">
+        <button className="btn btn-primary" onClick={() => authenticate()}>Authenticate</button>
+      </div>
+    )
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="container pt-5 text-center">
+      <h1>Welcome {user.get("username")}</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { MoralisProvider } from 'react-moralis'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const SERVERID = import.meta.env.VITE_MORALIS_SERVER_ID
+const SERVERURL = import.meta.env.VITE_MORALIS_SERVER_URL
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={SERVERID} serverUrl={SERVERURL}>
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
